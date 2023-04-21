51f06e304361834ea13804edb31926dcd18fe323
Felipe Perlato <fe_perlato@hotmail.com>
2023-04-21 18:09:33 -0300

    primeiro commit

---
+++ Dicionario/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+        cookies["user"] = "Maria";
+        cookies["password"] = "joao";
+
+        cookies["fone"] = "45998071677";
+
+
+
+        Console.WriteLine(cookies["user"]);
+
+    
+
+        Console.WriteLine("todos os cookies " );
+
+        //mostrar todos os valores
+
+        foreach (KeyValuePair<string, string> item in cookies) 
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+        //ou
+        foreach (var item in cookies)
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+
+
+
+    }
+}
